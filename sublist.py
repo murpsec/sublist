77e0fa9d49f9a40ce507f4c9ad043f5e92c046f9
murpsec <42382645+murpsec@users.noreply.github.com>
2018-10-10 12:06:38 -0400

    Add files via upload

---
+++ sublist.py
@@ -0,0 +1,62 @@
+#! python3
+#  sublist v1.0
+#  Authors:
+#  Brent Murphy - twitter.com/brent_murphy
+#  Biran Patel - twitter.com/_nopreserveroot
+  
+import requests
+import types
+import sys
+from bs4 import BeautifulSoup
+ 
+print("""
+ __       _       __ _     _   
+/ _\_   _| |__   / /(_)___| |_ 
+\ \| | | | '_ \ / / | / __| __|
+_\ \ |_| | |_) / /__| \__ \ |_ 
+\__/\__,_|_.__/\____/_|___/\__|
+                                
+# Created By @brent_murphy and @_nopreserveroot
+    """ )
+
+#remove duplicates
+def quickRemoveDuplicates(lst):
+	d={}
+	for elem in lst:
+		if elem not in d.keys():
+			d[elem]=1
+		else:
+			d[elem]+=1
+	return d.keys()
+
+# user specified input
+url = input( "Please enter a valid domain (example: test.com): ")
+
+r = requests.get('https://crt.sh?q=%25.'+url)
+html_content = r.text
+  
+# parse the html using beautiful soup
+soup = BeautifulSoup(html_content,"html.parser")
+  
+#data from correct table
+secondtable = soup.findAll('table')[1]
+out=[] 
+for row in secondtable.findAll("tr"):
+  data = []
+  if type (row) is list:
+    data.append(row.findAll("td"))
+  else:
+    data += row
+  if len(data) < 13:
+    continue
+  out.append (str(data[9]).strip("<td></td><th></th>"))
+
+out = list(quickRemoveDuplicates(out))
+if len(out) <= 0:
+    print ("Oops, invalid domain! Re-run with a valid domain.")
+    sys.exit()
+else:
+  while "Identity" in out:
+	  out.remove("Identity")
+for i in out:
+  print (i)
